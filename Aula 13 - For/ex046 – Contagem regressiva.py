326f9b2a74feed932439645b79ca023139f19ee4
Wanderson Santos <81826043+wandersonDeve@users.noreply.github.com>
2021-06-29 08:28:48 -0300

    Aula 13 - ex46

---
+++ Aula 13 - For/ex046 – Contagem regressiva.py
@@ -0,0 +1,11 @@
+from time import sleep
+print('==== CONTAGEM REGRESSIVA ====')
+
+print('''A queima de fogos logo sera inicada ....
+contagem regressiva''')
+sleep(2)
+
+for seg in range(10,-1,-1):
+    print(seg)
+    sleep(1)
+print('QUE LINDO FOGOS')
